22bc936c948b37472f4273082a51da4e0ad37544
mirnaghi7175 <mirnaghi7175@gmail.com>
2020-05-17 15:20:44 +0400

    prettify App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,15 @@ import "./App.css";
 import BusinessList from '../BusinessList/BusinessList.js';
 import SearchBar from '../SearchBar/SearchBar.js';
 
-class App extends React.Component{
-  render(){
+class App extends React.Component {
+  render() {
     return (
-    <div className="App">
-    <h1>ravenous</h1>
-    <SearchBar />
-    <BusinessList />
-</div>
-  );
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
   }
 
 }
