d3293016596283d8d8aefdc96ef24c5fead7fbae
mirnaghi7175 <mirnaghi7175@gmail.com>
2020-05-17 15:12:48 +0400

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import BusinessList from '../BusinessList/BusinessList.js';
+import SearchBar from '../SearchBar/SearchBar.js';
+
+class App extends React.Component{
+  render(){
+    return (
+    <div className="App">
+    <h1>ravenous</h1>
+    <SearchBar />
+    <BusinessList />
+</div>
+  );
+  }
+
+}
+
+export default App;
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions = {
+    "Best Match": "best_match",
+    "Highest Rated": "rating",
+    "Most Reviewed": "review_count"
+};
+
+class SearchBar extends React.Component{
+    renderSortByOptions(){
+        Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOptions[sortByOption];
+            return <li key={sortByOptionValue} > {sortByOption} </li>;
+        })
+    }
+
+    render(){
+        return (
+            <div class="SearchBar">
+              <div class="SearchBar-sort-options">
+                <ul>
+                  {this.renderSortByOptions()}
+                </ul>
+              </div>
+              <div class="SearchBar-fields">
+                <input placeholder="Search Businesses" />
+                <input placeholder="Where?" />
+              </div>
+              <div class="SearchBar-submit">
+                <a>Let's Go</a>
+              </div>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
