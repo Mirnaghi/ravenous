d38ddc53936c546556b6f6b025759d562a247946
Mirnaghi Aghazada <mirnaghi7175@gmail.com>
2020-05-19 02:06:53 +0400

    remove hard coded Business comp.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,18 +4,40 @@ import "./App.css";
 import BusinessList from '../BusinessList/BusinessList.js';
 import SearchBar from '../SearchBar/SearchBar.js';
 
+// business object
+const business = {
+    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 5.0,
+    reviewCount: 70
+};
+
+// businesses array
+let businesses = [
+    business,
+    business,
+    business,
+    business,
+    business,
+    business,
+];
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses}/>
       </div>
     );
   }
 
 }
-
 
 export default App;
